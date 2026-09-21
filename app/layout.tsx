f59e9608c0eea4dc7fcd6258c8b7c1e446c70477
s.tani <sta11432148@gmail.com>
2026-09-21 21:51:41 +0900

    style: change theme color to light green

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   colorScheme: "light",
-  themeColor: "#fffaf7",
+  themeColor: "#f7fbf5",
 }
 
 export default function RootLayout({
